e5bc857dffff57cda20730b730f8c2a30cad29e5
suyash-s09 <suyashs@apra.in>
2024-01-16 16:23:28 +0530

    first commit

---
+++ AdminLibrary/AdminLibrary/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminLibrary
+{
+    class Admin
+    {
+        string usename = "admin";
+        string password = "password";
+
+        bool validateAdmin(string username, string password)
+        {
+            return (this.usename == username && this.password == password);
+        }
+    }
+}
---
+++ UserLibrary/UserLibrary/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TaskLibrary;
+
+namespace UserLibrary
+{
+    public class User
+    {
+        private int userId;
+        private string password;
+        
+
+        public void SetUserId(int userId)
+        {
+            this.userId = userId;
+        }
+
+        public void SetUserPassword(string password) {
+            this.password = password;
+        }
+
+        public bool ValidateUser(int userId, string password)
+        {
+            return (this.userId==userId && this.password==password);
+        }
+
+       
+
+
+
+    }
+}
